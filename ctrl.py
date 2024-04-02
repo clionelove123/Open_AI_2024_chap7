a478150da82a6bd918b0138afbd7511033d0b23c
clione <clionelove@jejunu.ac.kr>
2024-04-03 08:30:39 +0900

    Modify div function using exception

--- ctrl.py
+++ ctrl.py
@@ -31,6 +31,8 @@ class Control:
         try:
             if(b==0):
                 raise Exception("Divisor Error")
+        except Exception as e:
+            return e
         return a/b
 
     def pow(self, a, b):
